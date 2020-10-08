8a1c197489fa9c130f0c6c8a94bb20d1b7aa170d
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2020-10-08 19:23:00 +0530

    Natours App made using gatsby

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Logo from "../img/logo-white.png";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-box">
+        <img src={Logo} alt="logo" className="logo" />
+      </div>
+
+      <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main"> Outdoors </span>
+          <span className="heading-primary-sub"> is where life happens </span>
+        </h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
