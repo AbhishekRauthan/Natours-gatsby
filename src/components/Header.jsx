82ba25fe1325658ae5a96f051c44c1af2bed8f58
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2020-10-09 16:57:27 +0530

    implemented bem architecture

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,17 @@ import Logo from "../img/logo-white.png";
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo-box">
-        <img src={Logo} alt="logo" className="logo" />
+      <div className="header__logo-box">
+        <img src={Logo} alt="logo" className="header__logo" />
       </div>
 
-      <div className="text-box">
+      <div className="header__text-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main"> Outdoors </span>
-          <span className="heading-primary-sub"> is where life happens </span>
+          <span className="heading-primary--main"> Outdoors </span>
+          <span className="heading-primary--sub"> is where life happens </span>
         </h1>
 
-        <a href="" className="btn btn-white btn-animated" onClick={e => e.preventDefault()}>Discover our tours</a>
+        <a href="" className="btn btn--white btn--animated" onClick={e => e.preventDefault()}>Discover our tours</a>
       </div>
     </header>
   )
