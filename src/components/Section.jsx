c2977d8b2584f46fb4ebdbf2239459fb97ef4e2c
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2020-10-10 17:04:09 +0530

    added 7-1 architecture and section.jsx

---
+++ src/components/Section.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <section className="section-about">
+      <div className="u-center-text">
+        <h2 className="heading-secondary">
+          Exciting tours for adventurous people
+        </h2>
+      </div>
+      <div className="row">
+        <div className="col-1-of-2">
+          Text Content
+        </div>
+        <div className="col-1-of-2">
+          image composition
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Section
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
+import Section from '../components/Section'
 import '../scss/app.scss'
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Section />
     </div>
   )
 }
