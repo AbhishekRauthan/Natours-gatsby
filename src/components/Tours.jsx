722a2d2386485313d4274cd272dac3d842daa06c
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2020-10-11 21:48:18 +0530

    continued working on tours section

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -12,29 +12,19 @@ const Tours = () => {
         <div className="col-1-of-3">
           <div className="card">
             <div className="card__side card__side--front">
-              SOME TEXT IN FRONT
+              <div className="card__pic card__pic--1">
+                &nbsp;
+              </div>
+              <h4 className="card__heading">
+                <span className="card__heading-span card__heading-span--1">
+                  The Sea Explorer
+                </span>
+              </h4>
+              <div className="card__details">
+                Details
+              </div>
             </div>
             <div className="card__side card__side--back card__side--back-1">
-              SOME TEXT IN BACK
-            </div>
-          </div>
-        </div>
-        <div className="col-1-of-3">
-          <div className="card">
-            <div className="card__side card__side--front">
-              SOME TEXT IN FRONT
-            </div>
-            <div className="card__side card__side--back card__side--back-2">
-              SOME TEXT IN BACK
-            </div>
-          </div>
-        </div>
-        <div className="col-1-of-3">
-          <div className="card">
-            <div className="card__side card__side--front">
-              SOME TEXT IN FRONT
-            </div>
-            <div className="card__side card__side--back card__side--back-3">
               SOME TEXT IN BACK
             </div>
           </div>
