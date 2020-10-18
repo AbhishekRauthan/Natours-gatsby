9bf456ec0f64528a7a6565cb02d932e5eccb7bda
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2020-10-18 22:26:45 +0530

    added section-stories

---
+++ src/components/Stories.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Stories = () => {
+  return (
+    <div className="section-stories">
+      <div className="u-center-text u-margin-bottom-big">
+        <h2 className="heading-secondary">
+          We make people genuinely happy
+        </h2>
+      </div>
+      <div className="row">
+        <div className="story">
+          <figure className="story__shape">
+          
+          </figure>
+          <div className="story__text">
+            <h3 className="heading-tertiary u-margin-bottom-small">
+              I had the best week ever with my family
+            </h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae harum quas Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel, culpa harum voluptate earum .</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Stories
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Feature from '../components/Feature';
 import Header from '../components/Header'
 import Section from '../components/Section'
+import Stories from '../components/Stories';
 import Tours from '../components/Tours';
 import '../scss/app.scss';
 import '../static/icon-fonts.css';
@@ -13,6 +14,7 @@ const Home = () => {
       <Section />
       <Feature />
       <Tours />
+      <Stories />
     </div>
   )
 }
