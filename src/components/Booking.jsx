2b338d18bedacd01abe7a965394c34c103fecd57
AbhishekRauthan <abhishek.rauthan.10@gmail.com>
2020-10-23 10:33:16 +0530

    completed booking section and fixed button

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -20,7 +20,7 @@ const Booking = () => {
                   className="form__input"
                   id="name"
                 />
-                <label htmlFor="name" className="form__label">Name: </label>
+                <label htmlFor="name" className="form__label">Name</label>
               </div>
               <div className="form__group">
                 <input
@@ -30,7 +30,28 @@ const Booking = () => {
                   className="form__input"
                   id="email"
                 />
-                <label htmlFor="email" className="form__label">Email: </label>
+                <label htmlFor="email" className="form__label">Email</label>
+              </div>
+              <div className="form__group">
+                <div className="form__radio-group">
+                  <input type="radio" className="form__radio-input" id="small" name="size" />
+                  <label htmlFor="small" className="form__radio-label">
+                    <span className="form__radio-button"></span>
+                    Small Tour Group
+                  </label>
+                </div>
+                <div className="form__radio-group">
+                  <input type="radio" className="form__radio-input" id="large" name="size" />
+                  <label htmlFor="large" className="form__radio-label">
+                    <span className="form__radio-button"></span>
+                    Large Tour Group
+                  </label>
+                </div>
+              </div>
+              <div className="form__group">
+                <button onClick={e => e.preventDefault()} className="btn btn--green">
+                  Next step &rarr;
+                </button>
               </div>
             </form>
           </div>
